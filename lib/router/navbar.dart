c90eb2bed9b61dd405ce71fc0d5fb0290c324455
belier <belier@belier.cn>
2025-12-03 16:19:12 +0800

    feat: Windows Linux 平台显示自定义的关闭、全屏、最小化按钮

--- lib/router/navbar.dart
+++ lib/router/navbar.dart
@@ -1,7 +1,11 @@
+import "dart:io";
+
 import "package:certimate/extension/index.dart";
 import "package:flutter/material.dart";
+import "package:flutter_hooks/flutter_hooks.dart";
 import "package:flutter_platform_widgets/flutter_platform_widgets.dart";
 import "package:go_router/go_router.dart";
+import "package:window_manager/window_manager.dart";
 
 class ScaffoldWithNavbar extends StatelessWidget {
   const ScaffoldWithNavbar(this.navigationShell, {super.key});
@@ -12,25 +16,39 @@ class ScaffoldWithNavbar extends StatelessWidget {
   Widget build(BuildContext context) {
     final s = context.s;
     if (isDesktopDevice) {
+      final navigationRail = NavigationRail(
+        selectedIndex: navigationShell.currentIndex,
+        onDestinationSelected: _onChange,
+        labelType: NavigationRailLabelType.selected,
+        leading: context.isCupertinoStyle
+            ? const SizedBox(height: 28)
+            : const SizedBox(height: 52),
+        destinations: [
+          NavigationRailDestination(
+            icon: Icon(context.platformIcons.home),
+            label: Text(s.home.titleCase),
+          ),
+          NavigationRailDestination(
+            icon: Icon(context.platformIcons.settings),
+            label: Text(s.settings.titleCase),
+          ),
+        ],
+      );
       return Scaffold(
         body: Row(
           children: [
-            NavigationRail(
-              selectedIndex: navigationShell.currentIndex,
-              onDestinationSelected: _onChange,
-              labelType: NavigationRailLabelType.selected,
-              leading: const SizedBox(height: 28),
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(context.platformIcons.home),
-                  label: Text(s.home.titleCase),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(context.platformIcons.settings),
-                  label: Text(s.settings.titleCase),
-                ),
-              ],
-            ),
+            Platform.isMacOS
+                ? navigationRail
+                : Stack(
+                    children: [
+                      navigationRail,
+                      const Positioned(
+                        top: 0,
+                        left: 0,
+                        child: _DesktopButtons(),
+                      ),
+                    ],
+                  ),
             Expanded(child: navigationShell),
           ],
         ),
@@ -60,6 +78,66 @@ class ScaffoldWithNavbar extends StatelessWidget {
     navigationShell.goBranch(
       index,
       initialLocation: index == navigationShell.currentIndex,
+    );
+  }
+}
+
+class _DesktopButtons extends HookWidget {
+  const _DesktopButtons();
+
+  @override
+  Widget build(BuildContext context) {
+    final isFullScreen = useRef(false);
+    final theme = context.theme;
+    final hoverColor = theme.brightness == Brightness.dark
+        ? theme.colorScheme.surfaceBright
+        : const Color(0xFFE5E5E5);
+    return SizedBox(
+      height: 28,
+      width: 80,
+      child: IconTheme(
+        data: theme.iconTheme.copyWith(size: 16),
+        child: Row(
+          children: [
+            Material(
+              child: SizedBox(
+                width: 26,
+                height: 26,
+                child: InkWell(
+                  onTap: () => windowManager.close(),
+                  hoverColor: const Color(0xFFE81122),
+                  child: const Icon(Icons.close, size: 18),
+                ),
+              ),
+            ),
+            Material(
+              child: SizedBox(
+                width: 26,
+                height: 26,
+                child: InkWell(
+                  onTap: () async {
+                    await windowManager.setFullScreen(!isFullScreen.value);
+                    isFullScreen.value = !isFullScreen.value;
+                  },
+                  hoverColor: hoverColor,
+                  child: const Icon(Icons.square_outlined),
+                ),
+              ),
+            ),
+            Material(
+              child: SizedBox(
+                width: 26,
+                height: 26,
+                child: InkWell(
+                  onTap: () => windowManager.hide(),
+                  hoverColor: hoverColor,
+                  child: const Icon(Icons.horizontal_rule),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
