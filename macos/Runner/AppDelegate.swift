d09e062d898a53ef77004a10b7dce59412fe8201
belier <belier@belier.cn>
2025-12-19 14:51:03 +0800

    fix: 修复 MacOS 窗口关闭后恢复时会显示 Colors 窗口的问题

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -12,15 +12,22 @@ class AppDelegate: FlutterAppDelegate {
     return true
   }
 
+  // https://github.com/macosui/macos_ui/issues/328#issuecomment-1403568232
   // https://leanflutter.dev/zh/blog/click-dock-icon-to-restore-after-closing-the-window
   override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
     if !flag {
       for window in NSApp.windows {
-        if !window.isVisible {
-          window.setIsVisible(true)
+        if (window.isFloatingPanel && window.title == "Colors") {
+          if (!window.isVisible) {
+            window.orderOut(self)
+          }
+        }else{
+          if !window.isVisible {
+            window.setIsVisible(true)
+          }
+          window.makeKeyAndOrderFront(self)
+          NSApp.activate(ignoringOtherApps: true)
         }
-        window.makeKeyAndOrderFront(self)
-        NSApp.activate(ignoringOtherApps: true)
       }
     }
     return true
